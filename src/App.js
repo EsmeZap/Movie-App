8b8930b943ba812d72749ef20b4836c99d981c3c
Esme <esmezapata99@gmail.com>
2024-01-07 17:21:28 -0700

    styles commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import { useEffect } from "react";
 
-//421b78e1
-
-const API URL = 'http://www.omdbapi.com?apikey=421b78e1';
+const API_URL = 'http://www.omdbapi.com?apikey=421b78e1';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect (() => {
+        searchMovies('Spiderman');
+
+    }, []);
+
     return(
         <h1>App</h1>
     );
